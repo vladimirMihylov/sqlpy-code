d16778124493fd65e41512bfdc103e1a0cbbf8ee
Vladimir Mihylov <vladimir.mhylv@yandex.ru>
2023-12-18 21:49:50 +0300

    Update tables.sql
    задание по второму уроку

--- 02/tables.sql
+++ 02/tables.sql
@@ -1,64 +1,46 @@
--- все, что начинается с двух дефисов - это комментарий
-
--- один к одному (1 вариант)
-
---CREATE TABLE IF NOT EXISTS Student (
---	email VARCHAR(80) PRIMARY KEY,
---	name VARCHAR(40) NOT NULL,
---	password VARCHAR(128) NOT NULL
---);
---
---CREATE TABLE IF NOT EXISTS StudentInfo (
---	email VARCHAR(80) PRIMARY KEY REFERENCES Student(email),
---	birthday date,
---	city VARCHAR(60),
---	roi TEXT
---);
-
--- один к одному (2 вариант)
-
-CREATE TABLE IF NOT EXISTS Student (
-	id SERIAL PRIMARY KEY,
-	email VARCHAR(80) UNIQUE NOT NULL,
-	name VARCHAR(40) NOT NULL,
-	PASSWORD VARCHAR(128) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS StudentInfo (
-	id INTEGER PRIMARY KEY REFERENCES Student(id),
-	birthday date,
-	city VARCHAR(60),
-	roi TEXT
-);
-
--- один ко многим
-
-CREATE TABLE IF NOT EXISTS Course (
+CREATE TABLE IF NOT EXISTS Genres (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(60) NOT NULL,
-	description TEXT
 );
 
-CREATE TABLE IF NOT EXISTS HomeworkTask (
+CREATE TABLE IF NOT EXISTS Performers (
 	id SERIAL PRIMARY KEY,
-	course_id INTEGER NOT NULL REFERENCES Course(id),
-	number INTEGER NOT NULL,
-	description TEXT NOT NULL
+	name VARCHAR(60) NOT NULL,
 );
 
--- многие ко многим (1 вариант)
-
-CREATE TABLE IF NOT EXISTS CourseStudent (
-	course_id INTEGER REFERENCES Course(id),
-	student_id INTEGER REFERENCES Student(id),
-	CONSTRAINT pk PRIMARY KEY (course_id, student_id)
+CREATE TABLE IF NOT EXISTS PerformersGenres (
+	performers_id INTEGER REFERENCES Performers(id),
+	genres_id INTEGER REFERENCES Genres(id),
+	CONSTRAINT pk PRIMARY KEY (performers_id, genres_id)
 );
 
--- многие ко многим (2 вариант)
-
-CREATE TABLE IF NOT EXISTS HomeworkSolution (
+CREATE TABLE IF NOT EXISTS Albums (
 	id SERIAL PRIMARY KEY,
-	task_id INTEGER NOT NULL REFERENCES HomeworkTask(id),
-	student_id INTEGER NOT NULL REFERENCES Student(id),
-	solution TEXT NOT NULL
+	name VARCHAR(60) NOT NULL,
+	year_of_release date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS AlbumsPerformers (
+	albums_id INTEGER REFERENCES Albums(id),
+  performers_id INTEGER REFERENCES Performers(id),
+	CONSTRAINT pk PRIMARY KEY (albums_id, performers_id)
+);
+
+CREATE TABLE IF NOT EXISTS Tracks (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	duration NUMERIC NOT NULL,
+  album_id INTEGER NOT NULL REFERENCES Albums(id)
+);
+
+CREATE TABLE IF NOT EXISTS Collections (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	year_of_release date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS CollectionsTracks (
+	collections_id INTEGER REFERENCES Collections(id),
+  tracks_id INTEGER REFERENCES Tracks(id),
+	CONSTRAINT pk PRIMARY KEY (collections_id, tracks_id)
 );
