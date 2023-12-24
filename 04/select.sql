446b2cafebf044d71e737333aac46adfad098a2b
Vladimir Mihylov <vladimir.mhylv@yandex.ru>
2023-12-24 15:27:48 +0300

    Update select.sql

--- 04/select.sql
+++ 04/select.sql
@@ -1,1 +1,17 @@
+-- Задание 2
+SELECT name, 
+duration
+FROM Tracks
+WHERE duration = (SELECT MAX(duration) FROM Tracks);
 
+SELECT name
+FROM Tracks
+WHERE duration >= 210;
+
+SELECT name
+FROM Collections
+WHERE EXTRACT(year RFOM year_of_release) BETWEEN '2018' AND '2020';
+
+SELECT name
+FROM Performers
+WHERE INSTR (name, ' ') > 0;
